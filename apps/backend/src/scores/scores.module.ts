2233f36f97ec0f30fda15f701341684f500a2282
kkeerl <kkeerl@tlu.ee>
2026-06-10 11:24:23 +0300

    fix(backend): fixed scores API implementation

--- apps/backend/src/scores/scores.module.ts
+++ apps/backend/src/scores/scores.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { DbModule } from '../db/db.module';
 import { ScoresController } from './scores.controller';
 import { ScoresService } from './scores.service';
 
 @Module({
-  imports: [DbModule],
+  imports: [DbModule, AuthModule],
   controllers: [ScoresController],
   providers: [ScoresService],
   exports: [ScoresService],
